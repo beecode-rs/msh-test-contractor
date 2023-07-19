4934e2de621eaf05a0e40507cc6bcdfb25640a47
milosbugarinovic <milos.bugarinovic@gmail.com>
2023-07-19 13:11:04 +0200

    refactor!: es modules and commonjs build
    BREAKING CHANGE: changed file structure

--- lib/util/fn-util.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const fnUtil: {
-    isConstructor: (fnName: string) => boolean;
-};
-//# sourceMappingURL=fn-util.d.ts.map
--- src/subject/subject-service.contract.ts
+++ src/subject/subject-service.contract.ts
@@ -1,53 +1,53 @@
-import { contractFactory } from '../contract/contractor-factory'
-import { SpecialFnName } from '../enum/special-fn-name'
-import { mocker } from '../mocker/mocker'
-import { ContractMockRevertFns } from '../types'
-import subjectClassFunctionStrategyContract from './subject-class-function-strategy.contract'
-import subjectConstructorStrategyContract from './subject-constructor-strategy.contract'
-import subjectFunctionStrategyContract from './subject-function-strategy.contract'
+import { contractFactory } from '#/contract/contractor-factory'
+import { SpecialFnName } from '#/enum/special-fn-name'
+import { mocker } from '#/mocker/mocker'
+import subjectClassFunctionStrategyContract from '#/subject/subject-class-function-strategy.contract'
+import subjectConstructorStrategyContract from '#/subject/subject-constructor-strategy.contract'
+import subjectFunctionStrategyContract from '#/subject/subject-function-strategy.contract'
+import { ContractMockRevertFns } from '#/types/index'
 
 class DummyClass {
-  a(_a: string): string {
-    return _a
-  }
+	a(_a: string): string {
+		return _a
+	}
 }
 
 const dummyContract = {
-  module: { DummyClass },
-  subjectName: 'DummyClass',
+	module: { DummyClass },
+	subjectName: 'DummyClass',
 }
 
 export default contractFactory(
-  { module: require('./subject-service'), subjectName: 'subjectService' },
-  {
-    strategyFromContractFunction: {
-      terms: [
-        {
-          mock: (): ContractMockRevertFns => {
-            return [mocker.contract(subjectConstructorStrategyContract).mockRestore]
-          },
-          params: [{ contract: dummyContract, fnName: SpecialFnName.CONSTRUCTOR, term: {} }],
-          result: { _module: dummyContract.module, _subjectName: dummyContract.subjectName },
-        },
-        {
-          mock: (): ContractMockRevertFns => {
-            return [mocker.contract(subjectClassFunctionStrategyContract).mockRestore]
-          },
-          params: [{ contract: dummyContract, fnName: 'a', term: { constructorParams: [] } }],
-          result: {
-            _module: dummyContract.module,
-            _subjectName: dummyContract.subjectName,
-            _fnName: 'a',
-          },
-        },
-        {
-          mock: (): ContractMockRevertFns => {
-            return [mocker.contract(subjectFunctionStrategyContract).mockRestore]
-          },
-          params: [{ contract: dummyContract, fnName: 'a', term: {} }],
-          result: { _module: dummyContract.module, _subjectName: dummyContract.subjectName, _fnName: 'a' },
-        },
-      ],
-    },
-  }
+	{ module: require('./subject-service'), subjectName: 'subjectService' },
+	{
+		strategyFromContractFunction: {
+			terms: [
+				{
+					mock: (): ContractMockRevertFns => {
+						return [mocker.contract(subjectConstructorStrategyContract).mockRestore]
+					},
+					params: [{ contract: dummyContract, fnName: SpecialFnName.CONSTRUCTOR, term: {} }],
+					result: { _module: dummyContract.module, _subjectName: dummyContract.subjectName },
+				},
+				{
+					mock: (): ContractMockRevertFns => {
+						return [mocker.contract(subjectClassFunctionStrategyContract).mockRestore]
+					},
+					params: [{ contract: dummyContract, fnName: 'a', term: { constructorParams: [] } }],
+					result: {
+						_fnName: 'a',
+						_module: dummyContract.module,
+						_subjectName: dummyContract.subjectName,
+					},
+				},
+				{
+					mock: (): ContractMockRevertFns => {
+						return [mocker.contract(subjectFunctionStrategyContract).mockRestore]
+					},
+					params: [{ contract: dummyContract, fnName: 'a', term: {} }],
+					result: { _fnName: 'a', _module: dummyContract.module, _subjectName: dummyContract.subjectName },
+				},
+			],
+		},
+	}
 )
