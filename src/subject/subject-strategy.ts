098b3315da32c896ae09accc3c1fa3fa4a9644d6
milosbugarinovic <milos.bugarinovic@gmail.com>
2023-07-19 13:11:04 +0200

    refactor!: es modules and commonjs build
    BREAKING CHANGE: changed file structure

--- src/subject/subject-strategy.ts
+++ src/subject/subject-strategy.ts
@@ -1,9 +1,9 @@
-import { ContractTerm } from '../types'
+import { ContractTerm } from '#/types/index'
 
 export type SubjectFromContract = { subjectName: string; module: any }
 // export type SubjectFromContract = Pick<AnyContract, 'subjectName' | 'module'>
 
 export interface SubjectStrategy {
-  exec: (term: ContractTerm) => any
-  fn: () => any
+	exec: (term: ContractTerm) => any
+	fn: () => any
 }
