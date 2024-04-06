05cbddf91071d1c67998ebde41173ba806f0af64
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-04-06 21:34:31 +0200

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig: JestConfigWithTsJest = {
 	clearMocks: true,
 	maxConcurrency: 1,
+	moduleNameMapper: {
+		'^#src$': '<rootDir>/src',
+		'^#src/(.*)$': '<rootDir>/src/$1',
+	},
 	preset: 'ts-jest/presets/default-esm',
 	setupFilesAfterEnv: ['jest-extended/all', '<rootDir>/src/index-jest-setup.ts'],
 	testEnvironment: 'node',
