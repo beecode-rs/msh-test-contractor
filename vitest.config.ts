45fa9b55321b35ff21f6dbf780df3d0bf11be2d1
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-05-03 21:02:57 +0200

    fix: test coverage with vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
+		coverage: {
+			exclude: ['lib/**', ...coverageConfigDefaults.exclude],
+			provider: 'istanbul',
+		},
 		mockReset: true,
 		passWithNoTests: true,
 		setupFiles: ['./src/__tests__/index-jest-setup.ts'],
