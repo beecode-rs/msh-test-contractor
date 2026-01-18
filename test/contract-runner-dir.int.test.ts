ee037409d5167a10ca4bcfa83c54a7a4594c3d06
milosbugarinovic <milos.bugarinovic@gmail.com>
2026-01-18 23:19:21 +0100

    fix: replace logger with console for error output

--- test/contract-runner-dir.int.test.ts
+++ test/contract-runner-dir.int.test.ts
@@ -1,7 +1,7 @@
 import { contractorTestRunner } from '../src/contract/contractor-test-runner.js'
-import { logger } from 'logger.js'
 
 contractorTestRunner.dir('./test').catch((err: unknown) => {
-	logger.debug(`Error running tests:${JSON.stringify(err)}`)
+	// eslint-disable-next-line no-console
+	console.debug('Error running tests:', err)
 })
 // contractorTestRunner.file('./test/simple-function.contract.ts')
